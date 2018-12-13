32859a30cccd40204a17c1b569f413c75cb68cbf
joaofcmb <joaofernandomeireles@gmail.com>
2018-12-13 19:11:14 +0000

    Started working on responsive design for the whole website

--- templates/tpl_common.php
+++ templates/tpl_common.php
@@ -10,10 +10,10 @@
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
             <link href="https://fonts.googleapis.com/css?family=Exo:700|Montserrat|Muli|PT+Sans|Permanent+Marker|Poppins:500" rel="stylesheet">
 
-            <link rel="stylesheet" type="text/css" media="screen" href="../css/common.css" />
             <?php foreach($css_files as $css_file) { ?>
                 <link rel="stylesheet" type="text/css" media="screen" href="../css/<?=$css_file?>" />
             <?php } ?>
+            <link rel="stylesheet" type="text/css" media="screen" href="../css/common.css" />
 
             <script src="../js/main.js" defer></script>
             <?php foreach($js_files as $js_file) { ?>
